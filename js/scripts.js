384f8d2bd6700f4fdc57d378fe266627fe388a16
markdan58 <mark.dan@strathmore.edu>
2018-09-20 11:30:04 +0300

    change addition to subtraction

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// Business (or back-end) logic:
+
+var add = function (number1, number2) {
+    return number1 + number2;
+};
+var subtract = function (number1, number2) {
+    return number1 - number2;
+};
+var multiply = function (number1, number2) {
+    return number1 * number2;
+};
+var divide = function (number1, number2) {
+    return number1 / number2;
+};
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function () {
+    $("form#subtract").submit(function (event) {
+        event.preventDefault()
+        var number1 = parseInt($("#subtract1").val());
+        var number2 = parseInt($("#subtract2").val());
+        var result = subtract(number1, number2);
+        $("#output").text(result);
+    });
+    $("a").click(function(event){
+        event.preventDefault();
+    });
+});
